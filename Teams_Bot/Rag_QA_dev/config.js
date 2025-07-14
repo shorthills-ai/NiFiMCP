3d197cda52ab9f519f0bb30f945d9214fd7933b3
SHTLPMac098 <shtlpmac098@SHTLPMac098s-MacBook-Pro.local>
2025-07-14 13:41:38 +0530

    Made a folder so that all the user can view and upload their teams bot code

---
+++ Teams_Bot/Rag_QA_dev/config.js
@@ -0,0 +1,7 @@
+const config = {
+    MicrosoftAppId: "b4af8c2b-f217-4a5c-85ac-2438972617e0",
+    MicrosoftAppType: "SingleTenant",
+    MicrosoftAppTenantId: "85a77b6c-a790-4bcf-8fd6-c1f891dd360b",
+    MicrosoftAppPassword: "79K8Q~a6YXVJD2blHPLeEIJMsdayh1c5OoQrJbad",
+  };
+  module.exports = config;
